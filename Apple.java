f8f29a15b0c3831d4758d8e7f9ea68bb57c96d43
Anay-Alpha <349196303@gapps.yrdsb.ca>
2025-11-05 14:25:35 -0500

    moreObjects

---
+++ Apple.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class Apple extends Actor
+{
+
+    public void act()
+    {
+        move(-10);
+        if(getX() <= 0)
+        {
+            resetApple();
+        }
+        
+        
+        if(isTouching(Hero.class))
+        {
+            SadFace sadFace = new SadFace();
+            getWorld().addObject(sadFace, 300, 200); 
+            getWorld().removeObject(this);
+        }
+        
+
+    }
+    
+    public void resetApple()
+    {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0)
+        {
+            setLocation(600, 100);
+        }
+        else
+        {
+            setLocation(600, 300);
+        }
+    }
+}
